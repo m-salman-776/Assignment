b101a25d9978c571b3057ef1f7aef13735572b92
Salman <salman.anssari77@gmail.com>
2021-02-24 09:50:28 +0530

    Basic Setup

---
+++ Assignment/src/StringCalculator/Calculator.java
@@ -0,0 +1,9 @@
+package StringCalculator;
+
+public class Calculator {
+	public static void main(String[] args) {
+		StringCalculator sc = new StringCalculator();
+		int ans = sc.Add("");
+		System.out.print(ans);
+	}	
+}
---
+++ Assignment/src/StringCalculator/StringCalculator.java
@@ -0,0 +1,10 @@
+package StringCalculator;
+
+public class StringCalculator {
+	public  int Add(String numbers) {
+		int sum = 0;
+		int string_length = numbers.length();
+		if(string_length == 0) return sum;
+		return sum;
+	}
+}
