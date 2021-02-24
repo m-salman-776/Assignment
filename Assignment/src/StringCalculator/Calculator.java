e6b7783e75be61317d08b5e1c2b0125c4d2cb6ab
Salman <salman.anssari77@gmail.com>
2021-02-24 12:29:17 +0530

    Requirement 1 is completed
    Now the Method is able to fulfill the mentioned requirement it able
    handle upto three comma separated values

--- Assignment/src/StringCalculator/Calculator.java
+++ Assignment/src/StringCalculator/Calculator.java
@@ -1,9 +1,22 @@
 package StringCalculator;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class Calculator {
-	public static void main(String[] args) {
-		StringCalculator sc = new StringCalculator();
-		int ans = sc.Add("");
-		System.out.print(ans);
-	}	
+	@Test
+	public void emptyString() {
+		assertEquals(0,StringCalculator.add(""));
+	}
+	@Test
+	public void singleValue() {
+		assertEquals(1,StringCalculator.add("1"));
+	}
+	@Test
+	public void twoCommaSeparatedValues() {
+		assertEquals(12,StringCalculator.add("6,6"));
+	}
+	@Test
+	public void threeCommaSeparatedValues() {
+		assertEquals(34,StringCalculator.add("1,30,3"));
+	}
 }
--- Assignment/src/StringCalculator/StringCalculator.java
+++ Assignment/src/StringCalculator/StringCalculator.java
@@ -1,10 +1,23 @@
 package StringCalculator;
 
 public class StringCalculator {
-	public  int Add(String numbers) {
-		int sum = 0;
-		int string_length = numbers.length();
-		if(string_length == 0) return sum;
-		return sum;
+	public static int add(String input) {
+		if(input.isEmpty()) {
+			return 0;
+		}
+		else {
+			String numbers[] = input.split(",");
+//			if(numbers.length == 1) return stoi(numbers[0]);
+//			else return stoi(numbers[0]) + stoi(numbers[1]);
+			
+			int sum = 0;
+			for(String num : numbers) {
+				sum += stoi(num);
+			}
+			return sum ;
+		}
+	}
+	private static int stoi(String input) {
+		return Integer.parseInt(input);
 	}
 }
